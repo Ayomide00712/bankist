6281f7b35a8bc4d6dc3810e47ba2b16fe0e49b0d
Alkassim <usmanalkassim01@gmail.com>
2025-02-23 10:56:02 -0800

    added tabbed component for section 2

--- script.js
+++ script.js
@@ -33,26 +33,67 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-const header = document.querySelector('.header');
-const allSections = document.querySelectorAll('.section');
+// const header = document.querySelector('.header');
+// const allSections = document.querySelectorAll('.section');
 
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
+// message.innerHTML =
+//   'we use cookie for improved functionality and analytics. <button class = "btn btn--close-cookie"> Got it !<button> ';
 
-message.innerHTML =
-  'we use cookie for improved functionality and analytics. <button class = "btn btn--close-cookie"> Got it !<button> ';
+//   // header.prepend(message);
+//   header.append(message);
 
-  // header.prepend(message);
-  header.append(message);
+//   message.style.backgroundColor = 'black';
+//   message.style.width = '120%';
 
-  message.style.backgroundColor = 'black';
-  message.style.width = '120%';
+//   message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
 
-  message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+//   document.documentElement.style.setProperty('--color-primary', 'orangered')
 
-  document.documentElement.style.setProperty('--color-primary', 'orangered')
+//   const logo = document.querySelector('.nav__logo');
+//   console.log(logo.alt);
+//   console.log(logo.src);
 
-  const logo = document.querySelector('.nav__logo');
-  console.log(logo.alt);
-  console.log(logo.src);
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+document
+  .querySelector('.nav__links')
+  .addEventListener('click', function (e) {});
+
+document.querySelectorAll('.nav__link').forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    const id = this.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  
+
+  if (!clicked) return;
+
+  // remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'))
+  // Activate classes 
+  clicked.classList.add('operations__tab--active');
+
+  
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
